e67d877890c5d0885d7b634be3ddde6f936fb637
MSpringe07 <146367143+MSpringe07@users.noreply.github.com>
2024-08-13 12:07:48 +0000

    feat: Add user registration functionality

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "Successfully registered!"
+    else
+      flash.now[:alert] = @user.errors.full_messages.to_sentence
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @user = current_user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :username, :password, :password_confirmation)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :results
+  has_many :created_quizzes, class_name: 'Quiz', foreign_key: 'creator_id'
   validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
+
+  attr_accessor :password_confirmation
 end
